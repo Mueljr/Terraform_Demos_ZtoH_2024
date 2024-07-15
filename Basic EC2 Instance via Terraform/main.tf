fd5c9541fad3f4a06ff5e4ba70c2b7f01c3a5685
Samuel Adejumo <muellam99@gmail.com>
2024-07-15 16:10:03 +0100

    Update main.tf

--- Basic EC2 Instance via Terraform/main.tf
+++ Basic EC2 Instance via Terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "basic-ec2-terraform" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
-  key_name = "key-instance1"
+  key_name = "$keypairname"
 }

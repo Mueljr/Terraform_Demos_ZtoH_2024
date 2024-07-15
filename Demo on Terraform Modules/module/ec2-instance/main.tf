952f89f42d14c2ae26880ea99d689cfff371f184
Samuel Adejumo <muellam99@gmail.com>
2024-07-15 16:51:41 +0100

    Delete Demo on Terraform Modules/module/ec2-instance directory

--- Demo on Terraform Modules/module/ec2-instance/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "ec2-instance-module-teamA" {
-  ami = var.ami_value
-  instance_type = var.instance_type_value
-  key_name = var.key_name_value
-}
--- Demo on Terraform Modules/module/ec2-instance/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public-ip-address" {
-  value = aws_instance.ec2-instance-module-teamA.public_ip
-}
